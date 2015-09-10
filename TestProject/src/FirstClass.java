9f60bbf5aed4d8a0d7ca932c6cbcae05241cf427
Philipp Bergt <philipp.bergt@st.ovgu.de>
2015-09-10 10:18:02 +0200

    - first commit

---
+++ TestProject/src/FirstClass.java
@@ -0,0 +1,10 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lets write some stupid things!");
+		System.out.println("blabla!");
+	}
+
+}
